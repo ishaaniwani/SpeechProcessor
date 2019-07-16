36022166cabffee6b1e7ac245d656b86725f92db
Ishaan Sinha <ishaansinha@Ishaans-MacBook-Air.local>
2019-07-16 13:16:14 -0700

    added comments

--- __audio_array__.py
+++ __audio_array__.py
@@ -1,12 +1,25 @@
 from pydub import AudioSegment
 
 class AudioArray(object):
+    '''
+    Constructs an array of AudioSegment objects. If not array is passed in, then there will be a blank array 
+    array - array of AudioSegment objects to define the wrapper array
+    '''
     def __init__(self, array = []):
         self.sounds = array
 
+    '''
+    Appends an Audiosegment Object to the array 
+    audio - the AudioSegment object 
+    '''
     def add(self, audio):
         self.sounds.append(audio)
 
+    ''' 
+    Splits the wav file into 250 millesecond chunks, finds the absolute value of max DB in that chunk,
+    and then divides by the number of chunks, resulting in the normalized mean amplitude of the wav file 
+    audio - the AudioSegment object to be processed for the aforementioned 
+    '''
     def getNormalizedMeanAmplitude(self, audio):
         sounds = list(audio[::250])
         normalizedMeans = []
@@ -16,6 +29,9 @@ class AudioArray(object):
             return 0
         return sum(normalizedMeans)/len(normalizedMeans)
     
+    '''
+    Returns the index of the AudioSegment object that has the greatest normalizedMeanAmplitude 
+    '''
     def getMaxNormalizedAmplitude(self): 
         max = 0
         for i in range(len(self.sounds)): 
@@ -23,14 +39,24 @@ class AudioArray(object):
                 max = i 
         return max
 
+    '''
+    Overlays all the sound signals together. Also uses the weighting function to reduce dB response from other wav files. 
+    max - the index of the AudioSegment object with the greatest normalized mean amplitude. Does not negatively affect weighting 
+    of this one. 
+    '''
     def overlaySounds(self, max): 
         combinedSignals = self.sounds[max] 
         i = 0
         while i < len(self.sounds): 
             if i is not max: 
                 sound = self.sounds[i]
-                sound = sound - 25
+                sound = sound - self.weightedFunction(max , i)
                 combinedSignals = combinedSignals.overlay(sound)
             i = i + 1
         return combinedSignals
         
+    '''
+    Calculates the dB response to remove from the specified sound signel. This is for future reference. 
+    '''
+    def weightedFunction(self, max, i): 
+        return 25
--- __main__.py
+++ __main__.py
@@ -2,21 +2,33 @@ import speech_recognition as sr
 from pydub import AudioSegment
 from __audio_array__ import AudioArray
 
+'''
+Constructs AudioSegment objects 
+'''
 sound1 = AudioSegment.from_wav('wavfiles/can-you-keep-a-secret.wav')
 sound2 = AudioSegment.from_wav('wavfiles/crowd_laugh_1.wav')
 sound3 = AudioSegment.from_wav('wavfiles/footsteps-4.wav')
 
+'''
+Places AudioSegment objects into the wrapper class 
+'''
 sounds = AudioArray([sound1, sound2, sound3])
 
+'''
+Overlays signals without any processing to show what that is like 
+'''
 problem = sound1.overlay(sound2)
 problem = problem.overlay(sound3)
-problem.export('wavfiles/problem.wav',format='wav')
+problem.export('wavfiles/problem.wav', format='wav')
 
+'''
+Processes and overlays the signals to show what the solution is like 
+'''
 max = sounds.getMaxNormalizedAmplitude()
 solution = sounds.overlaySounds(max)
 solution.export('wavfiles/solution.wav', format='wav')
 
-''' Recognizing wav file '''
+''' Recognizing wav file (solution)'''
 r = sr.Recognizer()
 with sr.AudioFile('wavfiles/solution.wav') as source:
     audio = r.record(source)
